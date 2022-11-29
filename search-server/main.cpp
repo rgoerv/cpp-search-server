b8ccd8ae4e13fc1bdad855d2269460d12056b836
Роман <roma.eremeev.2014@mail.ru>
2022-11-29 19:57:57 +0300

    arbeit machen

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -90,54 +90,14 @@ void TestSortDocuments(){
     search_server.AddDocument(2, documents_.at(2), DocumentStatus::ACTUAL, {5, -12, 2, 1});
     search_server.AddDocument(3, documents_.at(3), DocumentStatus::BANNED, {9});
 
-    // Relevance computing
-    map<string, map<int, double>> word_to_document_freqs_;
-
-    for(const auto& [id, document] : documents_) {
-        const vector<string> words = SplitIntoWords(document);
-        const double inv_word_count = 1.0 / words.size();
-        
-        for (const string& word : words) {
-            word_to_document_freqs_[word][id] += inv_word_count;
-        }
-    }
-
-    map<int, double> document_to_relevance;
-
-    for (const string& word : SplitIntoWords("пушистый ухоженный кот"s)) {
-        if (word_to_document_freqs_.count(word) == 0) {
-            continue;
-        }
-        const double inverse_document_freq = log(search_server.GetDocumentCount() * 1.0 / word_to_document_freqs_.at(word).size());
-        for (const auto [document_id, term_freq] : word_to_document_freqs_.at(word)) {
-            document_to_relevance[document_id] += term_freq * inverse_document_freq;
-        }
-    }
-
-    auto ComputeRating = [](const vector<int> ratings)
-    { return accumulate(ratings.begin(), ratings.end(), 0) / static_cast<int>(ratings.size()); };
-
-    // Change arg func ComputeRating
-    vector<Document> answer_true = {{1, document_to_relevance.at(1), ComputeRating({7, 2, 7})},
-                                {0, document_to_relevance.at(0), ComputeRating({8, -3})},
-                                {2, document_to_relevance.at(2), ComputeRating({5, -12, 2, 1})}};
-
-    sort(answer_true.begin(), answer_true.end(),
-         [](const Document& lhs, const Document& rhs) {
-             const double epsilon = 1e-6;
-             if (abs(lhs.relevance - rhs.relevance) < epsilon) {
-                 return lhs.rating > rhs.rating;
-         } else {
-             return lhs.relevance > rhs.relevance;
-         }
-    });
-
     const vector<Document> anser_find = search_server.FindTopDocuments("пушистый ухоженный кот"s);
-    
-    ASSERT_EQUAL(answer_true.size(), answer_true.size());
-    const double epsilon = 1e-6;
-    for (size_t i = 0; i < answer_true.size(); ++i) {
-        ASSERT_EQUAL_HINT(anser_find[i].id, answer_true[i].id, "Sort by relevance perform not correctly."s);
+
+    double last_relevance = 0;
+    for (size_t i = 0; i < anser_find.size(); ++i) {
+        if(i != 0) {
+            ASSERT_HINT(last_relevance > anser_find[i].relevance, "Sort by relevance perform not correctly."s);
+        }
+        last_relevance = anser_find[i].relevance;
     }
 }
 
@@ -184,7 +144,7 @@ void TestComputingRelevance() {
 
     const vector<Document> anser_find = search_server.FindTopDocuments("пушистый ухоженный кот"s);
   
-    ASSERT_EQUAL(answer_true.size(), answer_true.size());
+    ASSERT_EQUAL(answer_true.size(), anser_find.size());
     const double epsilon = 1e-6;
     for (size_t i = 0; i < answer_true.size(); ++i) {
         ASSERT_HINT(abs(answer_true[i].relevance - anser_find[i].relevance) < epsilon, "Relevance calculated not correctly."s);
@@ -212,7 +172,7 @@ void TestComputingRating() {
 
     const vector<Document> anser_find = search_server.FindTopDocuments("пушистый ухоженный кот"s);
     
-    ASSERT_EQUAL(answer_true.size(), answer_true.size());
+    ASSERT_EQUAL(answer_true.size(), anser_find.size());
 
     for (size_t i = 0; i < answer_true.size(); ++i) {
         ASSERT_EQUAL_HINT(answer_true[i].rating, anser_find[i].rating, "Rating calculated not correctly."s);
