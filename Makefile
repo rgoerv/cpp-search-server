4c5c7557d8e03db4f66406d8dd89bd80077c82fc
rgoerv <roma.eremeev.2014@mail.ru>
2023-10-19 03:22:21 +0300

    refactoring and prepare

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	g++ ./search-server/main.cpp ./search-server/document.cpp ./search-server/process_queries.cpp ./search-server/read_input_functions.cpp ./search-server/remove_duplicates.cpp ./search-server/request_queue.cpp ./search-server/search_server.cpp ./search-server/string_processing.cpp ./search-server/generator.cpp ./search-server/tests.cpp -o search_server --std=c++17 
+	g++-12 ./search-server/main.cpp ./search-server/document.cpp ./search-server/process_queries.cpp ./search-server/read_input_functions.cpp ./search-server/remove_duplicates.cpp ./search-server/request_queue.cpp ./search-server/search_server.cpp ./search-server/string_processing.cpp ./search-server/generator.cpp ./search-server/tests.cpp -o search_server --std=c++17 -ltbb -lpthread -O2	
--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,7 +1,52 @@
+#include "process_queries.h"
+#include "search_server.h"
+#include "document.h"
+
 #include "tests.h"
 
-int main() {
-    TestSearchServer();
-    Benchmark();
-    system("pause");
+#include <execution>
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main(int argc, char *argv[]) {
+    if(argc < 2) {
+        /* Создается экзмепляр класса с конструктором, принимающий строку стоп слов. */
+        SearchServer search_server("and with"s);
+        int id = 0;
+        for (
+            const std::string& text : {
+                "white cat and yellow hat"s,
+                "curly cat curly tail"s,
+                "nasty dog with big eyes"s,
+                "nasty pigeon john"s,
+            }
+        ) {
+            /* Добавляем документы в базу. */
+            search_server.AddDocument(++id, text, DocumentStatus::ACTUAL, {1, 2});
+        }
+        std::cout << "ACTUAL by default:"s << std::endl;
+        /* Последовательная версия.
+        /* Выводим только актуальные документы. */
+        for (const Document& document : search_server.FindTopDocuments("curly nasty cat"s)) {
+            std::cout << document << std::endl;
+        }
+        std::cout << "BANNED:"s << std::endl;
+        /* Последовательная версия.
+        /* Только заблокированные документы. */
+        for (const Document& document : search_server.FindTopDocuments(std::execution::seq, "curly nasty cat"s, 
+            DocumentStatus::BANNED)) {
+            std::cout << document << std::endl;
+        }
+        std::cout << "Even ids:"s << std::endl;
+        /* Параллельная версия.
+        /* Документы с четным id. */
+        for (const Document& document : search_server.FindTopDocuments(std::execution::par, "curly nasty cat"s, 
+            [](int document_id, DocumentStatus status, int rating) { return document_id % 2 == 0; })) {
+            std::cout << document << std::endl;
+        }
+    } else {
+        TestSearchServer();
+        Benchmark();
+    }
 }
